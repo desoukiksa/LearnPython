a92ad5378e496562c3206f91624554c701f9712f
Dr. Mohamed El Desouki <mohamed_eldesouki@hotmail.com>
2022-10-25 13:05:11 +0300

    Add files via upload

---
+++ Python Project.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## 1- Import Necessary Libraries
+
+# In[ ]:
+
+
+
+
+
+# ## 2- Import the dataset and assign it to a data frame
+
+# In[ ]:
+
+
+
+
+
+# ## 3- Display All detailes about the Dataset (Info , describe, dtypes, head(),Tail(), ...etc)
+
+# In[ ]:
+
+
+
+
+
+# ## 4- Add new column Total to sum all the exams scores for each student
+
+# In[ ]:
+
+
+
+
+
+# ## 5- Make a separate data frame for male and another one for female students 
+# 
+
+# In[ ]:
+
+
+
+
+
+# ## 6- Find male students who pay the standard fees
+# 
+
+# In[ ]:
+
+
+
+
+
+# ## 7- Find How many Female student pay the 'free/reduced' fees
+# 
+
+# In[ ]:
+
+
+
+
+
+# ## 8- Find the mean score for math exam per male students 
+
+# In[ ]:
+
+
+
+
+
+# ## 9- Find the how many male and female sudents have passed the reading exam.
+# ### note: the pass score = 60
+
+# In[ ]:
+
+
+
+
+
+# ## 10- For each gender find the minimum and maximum scores in each exam
+
+# In[ ]:
+
+
+
+
+
+# ## 11- add new column Age to include each student age 
+
+# In[ ]:
+
+
+
+
+
+# ## 12- For each gender find the minimum and maximum Ages
+
+# In[ ]:
+
+
+
+
+
+# ## 13- find all parent_education and their frequencies
+
+# In[ ]:
+
+
+
+
+
+# ## 14- Replace the Fees value 'free/reduced' with 'Scholarship'
+# 
+
+# In[ ]:
+
+
+
+
